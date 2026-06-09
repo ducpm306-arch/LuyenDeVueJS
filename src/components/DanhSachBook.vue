cfcbd5bf4dac0a248d1f539dcd83c1f6c4e592a7
DELL <minhduc.59h3@gmail.com>
2026-06-09 22:12:28 +0700

    bruh

---
+++ src/components/DanhSachBook.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref, reactive } from "vue";
+
+defineProps({
+  msg: {
+    listBooks: Array,
+  },
+});
+</script>
+
+<template>
+  <table border="1">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Tên</th>
+        <th>Loại</th>
+        <th>Tác giả</th>
+        <th>Giá</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="b in listBooks" :key="b.listBooks">
+        <td>{{ b.id }}</td>
+        <td>{{ b.ten }}</td>
+        <td>{{ b.loai }}</td>
+        <td>{{ b.tacGia }}</td>
+        <td>{{ b.gia }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped></style>
